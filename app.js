4bede0f91068836a0214b05663f640abae8d781f
kwangheedesu <kwang5204@naver.com>
2022-07-03 10:33:17 +0900

    DoneCssWork
    DoneCssWork

---
+++ app.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+}
